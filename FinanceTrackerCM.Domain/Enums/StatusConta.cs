e133730f944d01d9e524599672d49ef00e218eb3
Claudio Matheus <claudio.silva@smartbreeder.com.br>
2026-06-09 09:43:06 -0300

    feat: Adicionar entidades e enums para gerenciamento de contas e transações

---
+++ FinanceTrackerCM.Domain/Enums/StatusConta.cs
@@ -0,0 +1,7 @@
+namespace FinanceTrackerCM.Domain.Enums{
+    public enum StatusConta
+    {
+        Ativa, // A conta está ativa e pode ser usada para transações
+        Inativa // A conta está inativa e não pode ser usada para transações
+    }
+}
