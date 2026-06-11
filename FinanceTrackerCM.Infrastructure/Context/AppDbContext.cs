8ed0e5894decdc11d28c844da7d66e2531f6643a
Claudio Matheus <claudiomatheus055@gmail.com>
2026-06-11 00:24:36 -0300

    feature: Finalizando context e infrastructure

--- FinanceTrackerCM.Infrastructure/Context/AppDbContext.cs
+++ FinanceTrackerCM.Infrastructure/Context/AppDbContext.cs
@@ -1,17 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using FinanceTrackerCM.Domain.Entities;
+using AuditLogCM.EFCore.Interceptors;
 
-namespace FinanceTrackerCM.Context.AppDbContext
+namespace FinanceTrackerCM.Infrastructure.Context
 {   
 
     public class AppDbContext : DbContext
     {
-        public DbSet<Transacao> Transacoes { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Conta> Contas { get; set; }
-        
-        
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        private readonly AuditInterceptor _auditInterceptor;
+
+
+        public DbSet<Transacao> Transacoes { get; set; } = null!;
+        public DbSet<Categoria> Categorias { get; set; } = null!;
+        public DbSet<Conta> Contas { get; set; } = null!;
+
+
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options,
+            AuditInterceptor auditInterceptor) : base(options)
         {
+            _auditInterceptor = auditInterceptor;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditInterceptor);
         }
     }
 
